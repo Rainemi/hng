8e3076ec79d288b5472979ec5e26c4b6869780a2
Rainemi <105657663+Rainemi@users.noreply.github.com>
2023-09-11 11:05:40 +0100

    Update settings.py

--- endpointjson_project/endpointjson_project/settings.py
+++ endpointjson_project/endpointjson_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-#gwh0i(mz@n3=f$!s(mn#-_%wg*ixm@6%7ufqq@elai#ic204a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
