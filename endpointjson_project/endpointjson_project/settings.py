b613c75cbaf86bb2579552a0004a42f9ca031d5e
Rainemi <105657663+Rainemi@users.noreply.github.com>
2023-09-11 12:34:53 +0100

    Update settings.py

--- endpointjson_project/endpointjson_project/settings.py
+++ endpointjson_project/endpointjson_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#gwh0i(mz@n3=f$!s(mn#-_%wg*ixm@6%7ufqq@elai#ic204a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['Rainemi.pythonanywhere.com']
 
 
 # Application definition
