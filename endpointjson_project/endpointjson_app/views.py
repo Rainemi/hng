b9024bc1132e3868f49d1422517924b75e2f023b
Rainemi <105657663+Rainemi@users.noreply.github.com>
2023-09-11 13:43:19 +0100

    Update views.py

--- endpointjson_project/endpointjson_app/views.py
+++ endpointjson_project/endpointjson_app/views.py
@@ -1,27 +1,27 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.views.decorators.http import require_GET
+from django.http import JsonResponse
 from datetime import datetime
-import os
+
 
 # Create your views here.
-
+@require_GET
 def Get_info(request):
     # Get query parameters from the request
     slack_name = request.GET.get('slack_name', 'Raine')
     track = request.GET.get('track', 'backend')
 
-    # Get the current day of the week and UTC time
+    # Current day of the week and UTC time
     current_day = datetime.utcnow().strftime('%A')
     current_utc_time = datetime.utcnow().isoformat(timespec='seconds')+ 'Z'
 
 
-    # Get the GitHub URL of the file being run
-    github_repo_url = 'https://github.com/rainemi/hng_task'
+    # GitHub URL of the file being run
+    github_repo_url = 'https://github.com/Rainemi/hng'
 
-    # Get the GitHub URL of the full source code (replace with your repository URL)
-    github_file_url = 'https://github.com/rainemi/hng_task/blob/master/'
+    # GitHub URL of the full source code (replace with your repository URL)
+    github_file_url = 'https://github.com/Rainemi/hng/blob/main/endpointjson_project/endpointjson_app/views.py'
 
-    # Create a dictionary to hold the response data
+    # Dictionary to hold the response data
     response_data = {
         'slack_name': slack_name,
         'current_day': current_day,
